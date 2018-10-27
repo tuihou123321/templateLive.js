d64125ae6709a07a0a7c9f51f1d7ccc0ca0ec1ae
tuihou123321 <chrome0423@gmail.com>
2018-10-27 15:46:57 +0800

    update

---
+++ Readme.md
@@ -0,0 +1,59 @@
+常用：
+
+函数类
+	1.
+f    fun(){}
+	2.
+f2   fun(a,()=>{})
+	3.
+fn   fun=()=>{}
+	4.
+fun   function fun(){}
+	5.
+ff  (function(){})()
+
+
+
+
+其他
+	1.
+log  console.log();
+	2.
+set  this.setState({key:value})
+	3.
+kv   key:value
+	4.
+lvr   let {value}=result;
+
+
+
+
+判断：if,else,elseif
+	1.
+if  if(){}
+	2.
+el  else{}
+	3.
+ife  if(){}else{}
+	4.
+eli   else if(){}
+	5.
+?   isTrue?a:b
+	6.
+|     isValue || defaltValue
+
+
+
+
+array
+	1.
+map
+	2.
+forEach
+	3.
+some
+	4.
+every
+	5.
+filter
+
---
+++ templateLive.js
@@ -0,0 +1,146 @@
+##webstorm代码简写
+
+import React, {Component} from 'react';
+export default class $Name$ extends Component {
+    state = {
+    }
+    componentDidMount(){
+    }
+    render() {
+        return (
+            <div>
+                $value$
+            </div>
+        );
+    }
+}
+//react,reactP
+
+
+import React, {Component} from 'react';
+export default function $Name$($arg$){
+    return (
+        <div>
+            $value$
+        </div>
+    );
+}
+//reactF
+
+
+
+this.setState({
+  $k$:$v$
+ })
+//  set
+
+$fun$($arg$){
+    $end$
+}
+// f
+
+$fun$($arg$,(result)=>{
+    $end$
+})
+// f2
+
+$fun$=($arg$)=>{
+    $end$
+}
+// fn
+
+function $fun$($arg$){
+    $end$
+}
+// fun
+
+
+(function () {
+    $end$
+})()
+//ff
+
+
+$isTrue$ ? $a$:$b$;
+//?
+
+$isTrue$ || $value$;
+// |
+
+let {$value$}=$result$;
+// lvr
+
+console.log($value$);
+// log
+
+
+$arr$.map((item,index)=>(
+    $end$
+))
+//map
+
+$arr$.forEach((item,index)=>(
+    $end$
+))
+//forEach
+
+
+$arr$.some((item,index)=>(
+    $end$
+))
+//some
+
+
+$arr$.every((item,index)=>(
+    $end$
+))
+//every
+
+$arr$.filter((item,index)=>(
+    $end$
+))
+//filter
+
+$key$:$vlaue$,
+//kv
+
+require("$module$")
+//req
+
+let $key$=$value$;
+//lkv
+
+
+if($bool$){
+    $then1$
+}
+//if
+
+else{
+    $then2$
+}
+//el
+
+else if($bool$){
+    $value$
+}
+//eli
+
+
+if($bool$){
+    $then1$
+}else{
+ $then2$
+}
+//ife
+
+
+if($bool$){
+    $then1$
+}else if($bool2$){
+    $then2$
+}else{
+    $then3$
+}
+//iel
+
